0942731250120d5db31275b1a2bfd7e61af3e33c
Doner <Doner@DONER->
2017-03-27 09:57:00 +0600

    ss

--- ConsoleApplication9/Program.cs
+++ ConsoleApplication9/Program.cs
@@ -4,12 +4,47 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApplication9
+namespace DelegateSample
 {
+    delegate void PrintDelegate();
+
+
+    class SuperClass
+    {
+        int a;
+        int b;
+
+        public int PrintOK(this)
+        {
+            return (this.a + this.b);
+       
+        }
+    }
+
+
+    class SimpleClass
+    {
+        public int a;
+        public int b;
+        public PrintDelegate pd;
+        public int PrintOK()
+        {
+            pd.Invoke(this);
+            return 0;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            
+            SuperClass c = new SuperClass();
+            SimpleClass s = new SimpleClass();
+            s.a = 6;
+            s.b = 3;
+            s.pd = c.PrintOK(this);
+            s.PrintOK(this);
         }
     }
 }
